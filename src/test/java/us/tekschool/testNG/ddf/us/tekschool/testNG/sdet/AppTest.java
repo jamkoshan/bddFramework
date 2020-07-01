cd135f9d17f112d79aabb410e89d8b3dd3621ee6
Jamshed <jamkoshan@gmail.com>
2020-07-01 18:56:06 -0400

    test

---
+++ src/test/java/us/tekschool/testNG/ddf/us/tekschool/testNG/sdet/AppTest.java
@@ -0,0 +1,20 @@
+package us.tekschool.testNG.ddf.us.tekschool.testNG.sdet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
